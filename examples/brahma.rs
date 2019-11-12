25ae805e3d5809eaac8b23b937ddb78acac1d65d
Nick Fitzgerald <fitzgen@gmail.com>
2019-11-11 20:40:07 -0800

    Add up to p9 from the brahma paper to the example

--- examples/brahma.rs
+++ examples/brahma.rs
@@ -6,7 +6,10 @@ fn main() {
 
     let context = z3::Context::new(&config);
 
-    let problems: Vec<fn(&z3::Context) -> Program> = vec![p1 as _, p2 as _, p23 as _];
+    let problems: Vec<fn(&z3::Context) -> Program> = vec![
+        p1 as _, p2 as _, p3 as _, p4 as _, p5 as _, p6 as _, p7 as _, p8 as _,
+        p9 as _, // p23 as _
+    ];
 
     for (i, p) in problems.into_iter().enumerate() {
         println!("========== p{} ==========", i + 1);
@@ -46,6 +49,102 @@ fn p2(context: &z3::Context) -> Program {
     let b = builder.const_(1);
     let c = builder.add(a, b);
     let _ = builder.and(a, c);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p3(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(0);
+    let c = builder.sub(b, a);
+    let _ = builder.and(a, c);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p4(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(1);
+    let c = builder.sub(a, b);
+    let _ = builder.xor(a, c);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p5(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(1);
+    let c = builder.sub(a, b);
+    let _ = builder.or(a, c);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p6(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(1);
+    let c = builder.add(a, b);
+    let _ = builder.or(a, c);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p7(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(0xffff_ffff_ffff_ffff);
+    let c = builder.xor(a, b);
+    let d = builder.const_(1);
+    let e = builder.add(c, d);
+    let _ = builder.and(c, e);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p8(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(1);
+    let c = builder.sub(a, b);
+    let d = builder.const_(0xffff_ffff_ffff_ffff);
+    let e = builder.xor(a, d);
+    let _ = builder.and(c, e);
+    let spec = builder.finish();
+
+    Program::synthesize(&context, &spec, &library).unwrap()
+}
+
+fn p9(context: &z3::Context) -> Program {
+    let library = Library::brahma_std();
+
+    let mut builder = ProgramBuilder::new();
+    let a = builder.var();
+    let b = builder.const_(31);
+    let c = builder.shru(a, b);
+    let d = builder.xor(a, c);
+    let _ = builder.sub(d, c);
     let spec = builder.finish();
 
     Program::synthesize(&context, &spec, &library).unwrap()
